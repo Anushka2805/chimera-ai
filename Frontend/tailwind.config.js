f0451bee45aadf06846795af8e9a9b4540eb7d05
Kritika Rana <kritikarana889@gmail.com>
2025-11-15 04:40:56 +0530

    Frontend made using html tailwind css and js

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Dashboard.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'cyan-glow': '#00bfff',
+        'red-glow': '#ff4d4d',
+      },
+    },
+  },
+  plugins: [],
+}
